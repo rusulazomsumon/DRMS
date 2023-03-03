79c49a14ecddab7feae879a6b3abc7a55e52723e
sumon <18103020@iubat.edu>
2023-03-03 16:20:50 +0600

    update: body content

---
+++ dashboard/index.php
@@ -0,0 +1,27 @@
+<?php
+    // session_start();
+    include("../connection.php");
+    include("../functions.php");
+    include("header.php");
+
+	// $user_data = check_login($conn);
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Dashboard</title>
+  </head>
+  <body>
+    <h1>Welcome!</h1>
+    <p>This is your dashboard page.</p>
+    
+    
+
+  </body>
+</html>
+
+<?php 
+  include("footer.php");
+?>
