fff0170e43e5373e33ce150b5372ec58d0151a4e
sumon <18103020@iubat.edu>
2023-03-05 12:23:26 +0600

    modified: picture showing added

--- single_result.php
+++ single_result.php
@@ -8,11 +8,11 @@
     $reg_id = $_GET['reg_id'];
 
     // select student info and their result from database
-    $query = "SELECT students.name, students.roll, results.marks_obtained, subjects.name AS subject_name
+    $query = "SELECT students.name, students.picture, students.roll, results.marks_obtained, subjects.name AS subject_name
             FROM students 
             INNER JOIN results ON students.s_id = results.s_id 
             INNER JOIN subjects ON results.sub_id = subjects.sub_id 
-            WHERE students.reg_id = '$reg_id' AND students.roll = '$roll'";
+            WHERE students.reg_id = '$reg_id' OR students.roll = '$roll'";
 
     $result = mysqli_query($conn, $query);
     if (!$result) {
@@ -31,6 +31,7 @@
 	<table>
 		<thead>
 			<tr>
+				<th>Picture</th>
 				<th>Name</th>
 				<th>Roll</th>
 				<th>Subject</th>
@@ -42,6 +43,7 @@
 			// loop through each row of data and display in table
 			while($row = mysqli_fetch_assoc($result)) {
 				echo "<tr>";
+				echo "<td><img src='/drms/dashboard/student/imgs/" . $row['picture'] . "' width='50' height='50'></td>";
 				echo "<td>" . $row['name'] . "</td>";
 				echo "<td>" . $row['roll'] . "</td>";
 				echo "<td>" . $row['subject_name'] . "</td>";
