06b5a94548e78b7ee5b10c9df697b5c8641a0fc1
sumon <18103020@iubat.edu>
2023-03-12 23:58:40 +0600

    design: dashbord

--- dashboard/index.php
+++ dashboard/index.php
@@ -16,7 +16,94 @@
   <body>
     
   <!-- #######################Admin Dashbaord############################# -->
-    
+    <div class="container">
+  <div class="row">
+    <div class="col-md-4">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">333</h5>
+          <p class="card-text">This is a description of the first card.</p>
+          <a href="#" class="card-link">See more</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Student</h5>
+          <p class="card-text">This is a description of the second card.</p>
+          <a href="#" class="card-link">See more</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Add new</h5>
+          <p class="card-text">This is a description of the third card.</p>
+          <a href="#" class="card-link">See more</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-4">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Title 4</h5>
+          <p class="card-text">This is a description of the fourth card.</p>
+          <a href="#" class="card-link">See more</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Title 5</h5>
+          <p class="card-text">This is a description of the fifth card.</p>
+          <a href="#" class="card-link">See more</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Title 6</h5>
+          <p class="card-text">This is a description of the sixth card.</p>
+          <a href="#" class="card-link">See more</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-4">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Title 7</h5>
+          <p class="card-text">This is a description of the seventh card.</p>
+          <a href="#" class="card-link">See more</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Title 8</h5>
+          <p class="card-text">This is a description of the eighth card.</p>
+          <a href="#" class="card-link">See more</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Title 9</h5>
+          <p class="card-text">This is a description of the eighth card.</p>
+          <a href="#" class="card-link">See more</a>
+        </div>
+      </div>
+    </div>
+
 
   </body>
 </html>
